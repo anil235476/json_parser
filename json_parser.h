7e2b1b7fb9de17404fb232c44838cb479cf24186
anil235476 <aks.dce.007@gmail.com>
2020-05-25 23:16:24 +0530

    support of making message for server

--- json_parser.h
+++ json_parser.h
@@ -172,6 +172,7 @@ namespace grt {
 	std::string make_participant_open_close(bool on);
 	std::string make_consumers_with_preferred_layers(std::vector<consumer_info> const& list);
 	std::string make_chat_open_close(bool on);
+	std::string make_consumers_with_preferred_layers(std::vector<consumer_info> const& list);
 }//namespace grt
 
 #endif//_JSON_PARSER_H__
